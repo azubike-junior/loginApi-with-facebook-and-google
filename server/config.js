753f6b73484483cee4031ab5693f7edd4c6ddbd9
zubi <azu@gmail.com>
2020-02-20 23:03:45 +0100

    added facebook url

--- server/config.js
+++ server/config.js
@@ -1,24 +1,15 @@
-const {
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    PORT,
-    FB_CLIENT_ID,
-    FB_CLIENT_SECRET
-} = process.env
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, PORT, FB_CLIENT_ID, FB_CLIENT_SECRET } = process.env;
 
 const passportGoogleConfig = {
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    clientID: GOOGLE_CLIENT_ID,
-    callbackURL: `https://login-api-v1.herokuapp.com/api/v1/auth/google/callback`
-}
+	clientSecret: GOOGLE_CLIENT_SECRET,
+	clientID: GOOGLE_CLIENT_ID,
+	callbackURL: `https://login-api-v1.herokuapp.com/api/v1/auth/google/callback`
+};
 
 const passportFacebookConfig = {
-    clientID: FB_CLIENT_ID,
-    clientSecret: FB_CLIENT_SECRET,
-    callbackURL: `http://localhost:${PORT}/api/v1/auth/facebook/callback`
-}
+	clientID: FB_CLIENT_ID,
+	clientSecret: FB_CLIENT_SECRET,
+	callbackURL: `https://login-api-v1.herokuapp.com/api/v1/auth/facebook/callback`
+};
 
-export {
-    passportFacebookConfig,
-    passportGoogleConfig
-}
+export { passportFacebookConfig, passportGoogleConfig };
