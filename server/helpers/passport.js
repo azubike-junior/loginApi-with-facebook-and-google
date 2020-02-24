dde354d9a37e24fb1f5663cdbf686cff44e41e0f
zubi <azu@gmail.com>
2020-02-24 13:27:35 +0100

    new

--- server/helpers/passport.js
+++ server/helpers/passport.js
@@ -23,17 +23,23 @@ const getAccessFromGoogle = (access, token, profile, done) => {
 const getAccessFromFacebook = (access, token, profile, done) => {
     process.nextTick(async () => {
         try {
-            const foundUser = await User.findUser(profile.email);
+            const {
+                id,
+                displayName,
+                email
+            } = profile
+            const foundUser = await User.findUser(email);
             console.log('foudder', foundUser)
-            if (foundUser) {
-                return done(null, foundUser)
+            if (!foundUser.length) {
+                const newUser = await new User({
+                    fb_id: id,
+                    fb_email: profile.emails[0].value,
+                    display_name: displayName
+                })
+                await User.save(newUser)
+                return done(null, newUser)
             }
-            const newUser = await new User({
-                id: profile.id,
-                email: profile.emails[0].value
-            })
-            await User.save(newUser)
-            return done(null, newUser)
+            return done(null, foundUser)
         } catch (e) {
             done(e, null)
         }
